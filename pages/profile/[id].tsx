6d5a96cd400356387169351647d231eb8b62e1b0
thryec <thryec@gmail.com>
2022-07-04 15:34:01 +0800

    implemented follow user

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -2,7 +2,10 @@ import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
 import { client, getProfiles, getPublications } from '../../api'
 import Image from 'next/image'
-import { profile } from 'console'
+import { ethers } from 'ethers'
+import ABI from '../../abi.json'
+
+const address = '0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d'
 
 const Profile = () => {
   const [profile, setProfile] = useState<any>()
@@ -13,18 +16,39 @@ const Profile = () => {
   const fetchProfile = async () => {
     try {
       const response = await client.query(getProfiles, { id: id }).toPromise()
-      console.log('profile: ', response.data.profiles.items[0])
+      // console.log('profile: ', response.data.profiles.items[0])
       setProfile(response.data.profiles.items[0])
 
       const publicationData = await client
         .query(getPublications, { id: id })
         .toPromise()
 
-      console.log('pub data: ', publicationData.data.publications.items)
+      // console.log('pub data: ', publicationData.data.publications.items)
       setPubs(publicationData.data.publications.items)
     } catch (err) {
       console.log('err', err)
     }
+  }
+
+  const followUser = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    const contract = new ethers.Contract(address, ABI, signer)
+
+    try {
+      const tx = await contract.follow([id], [0x0])
+      const txn = await tx.wait()
+      console.log('followed user successfully: ', txn)
+    } catch (err) {
+      console.log('error following user: ', err)
+    }
+  }
+
+  const connect = async () => {
+    const accounts = await window.ethereum.request({
+      method: 'eth_requestAccounts',
+    })
+    console.log('accounts: ', accounts)
   }
 
   useEffect(() => {
@@ -37,6 +61,9 @@ const Profile = () => {
 
   return (
     <div>
+      <button onClick={connect} className="bg-blue-200">
+        Connect Wallet
+      </button>
       {profile.picture ? (
         <Image
           src={profile.picture.original.url}
@@ -53,6 +80,9 @@ const Profile = () => {
         <p>Followers: {profile.stats.totalFollowers}</p>
         <p>Following: {profile.stats.totalFollowing}</p>
       </div>
+      <button onClick={followUser} className="bg-blue-200">
+        Follow User
+      </button>
       <div>
         {pubs.map((pub: any, index) => (
           <div key={index} className="p-10 border-t-2">
