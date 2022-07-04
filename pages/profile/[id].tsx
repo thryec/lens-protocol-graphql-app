e726e994ccc9ee5cdd37a30f1249fe9cb15c9f6a
thryec <thryec@gmail.com>
2022-07-04 15:06:24 +0800

    fetch and display user publications

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,19 +1,27 @@
 import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
-import { client, getProfiles } from '../../api'
+import { client, getProfiles, getPublications } from '../../api'
 import Image from 'next/image'
 import { profile } from 'console'
 
 const Profile = () => {
   const [profile, setProfile] = useState<any>()
+  const [pubs, setPubs] = useState([])
   const router = useRouter()
   const { id } = router.query
 
   const fetchProfile = async () => {
     try {
       const response = await client.query(getProfiles, { id: id }).toPromise()
-      console.log('res: ', response.data.profiles.items[0])
+      console.log('profile: ', response.data.profiles.items[0])
       setProfile(response.data.profiles.items[0])
+
+      const publicationData = await client
+        .query(getPublications, { id: id })
+        .toPromise()
+
+      console.log('pub data: ', publicationData.data.publications.items)
+      setPubs(publicationData.data.publications.items)
     } catch (err) {
       console.log('err', err)
     }
@@ -44,6 +52,13 @@ const Profile = () => {
         <p>{profile.bio}</p>
         <p>Followers: {profile.stats.totalFollowers}</p>
         <p>Following: {profile.stats.totalFollowing}</p>
+      </div>
+      <div>
+        {pubs.map((pub: any, index) => (
+          <div key={index} className="p-10 border-t-2">
+            {pub.metadata.content}
+          </div>
+        ))}
       </div>
     </div>
   )
