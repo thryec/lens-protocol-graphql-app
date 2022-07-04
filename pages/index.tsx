088e39c60a7c964af5512a33284d3ce2f377e0a7
thryec <thryec@gmail.com>
2022-07-04 14:34:33 +0800

    display profiles on ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import { useState, useEffect } from 'react'
 import { client, recommendProfiles } from '../api'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Home: NextPage = () => {
   const [profiles, setProfiles] = useState([])
@@ -8,8 +10,8 @@ const Home: NextPage = () => {
   const fetchProfiles = async () => {
     try {
       const response = await client.query(recommendProfiles).toPromise()
-      console.log({ response })
-      setProfiles(response.data.recommendProfiles)
+      console.log('response:', response.data)
+      setProfiles(response.data.recommendedProfiles)
     } catch (err) {
       console.log(err)
     }
@@ -19,7 +21,29 @@ const Home: NextPage = () => {
     fetchProfiles()
   }, [])
 
-  return <div className="flex justify-center">hello world</div>
+  return (
+    <div className="">
+      {profiles &&
+        profiles.map((profile: any, index) => (
+          <Link href={`/profile/${profile.id}`} key={index} passHref>
+            <div className="my-4">
+              {profile.picture ? (
+                <Image
+                  src={profile.picture.original.url}
+                  width="60px"
+                  height="60px"
+                  alt={profile.name}
+                />
+              ) : (
+                <div className="w-60 h-60 bg-black"></div>
+              )}
+              <h4 className="font-semibold">{profile.handle}</h4>
+              <p>{profile.bio}</p>
+            </div>
+          </Link>
+        ))}
+    </div>
+  )
 }
 
 export default Home
