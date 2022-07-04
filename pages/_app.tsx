2185d416bb426705ac2910fda1ff0522d7687645
thryec <thryec@gmail.com>
2022-07-04 14:58:45 +0800

    display profile  data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from 'next/app'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="p-40">
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </div>
   )
 }
--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -12,7 +12,7 @@ const Profile = () => {
   const fetchProfile = async () => {
     try {
       const response = await client.query(getProfiles, { id: id }).toPromise()
-      console.log('res: ', response)
+      console.log('res: ', response.data.profiles.items[0])
       setProfile(response.data.profiles.items[0])
     } catch (err) {
       console.log('err', err)
@@ -25,18 +25,26 @@ const Profile = () => {
     }
   }, [id])
 
+  if (!profile) return null
+
   return (
     <div>
       {profile.picture ? (
         <Image
           src={profile.picture.original.url}
-          width="60px"
-          height="60px"
+          width="200px"
+          height="200px"
           alt={profile.name}
         />
       ) : (
-        <div></div>
+        <div className="w-200 h-200"></div>
       )}
+      <div className="space-y-4">
+        <h4>{profile.handle}</h4>
+        <p>{profile.bio}</p>
+        <p>Followers: {profile.stats.totalFollowers}</p>
+        <p>Following: {profile.stats.totalFollowing}</p>
+      </div>
     </div>
   )
 }
